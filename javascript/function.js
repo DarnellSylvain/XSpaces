d95c02d1b0f12c759bdf826e413c06f813ed1ff7
Darnell Sylvain <darnellsylvain@gmail.com>
2021-08-26 13:02:53 +0100

    addded favicon

--- javascript/function.js
+++ javascript/function.js
@@ -1,45 +1,36 @@
 const nav = document.getElementById("navbar");
-const navlinks = document.getElementsByClassName("nav-link")
-const navbarToggler = document.getElementsByClassName("navbar-toggler-icon")[0]
-
-
-nav.addEventListener("click", () => console.log("Yo"))
+const navlinks = document.getElementsByClassName("nav-link");
+const navbarToggler = document.getElementsByClassName("navbar-toggler-icon")[0];
 
 changeStyle = () => {
-    nav.classList.add("navbarScroll")
-    nav.classList.add("shadow")
-    navbarToggler.classList.add("navbar-toggler-scroll")
-}
+  nav.classList.add("navbarScroll");
+  nav.classList.add("shadow");
+  navbarToggler.classList.add("navbar-toggler-scroll");
+};
 
-console.log(navbarToggler)
+console.log(navbarToggler);
 
 if (window.pageYOffset > 430) {
+  changeStyle();
 
-    changeStyle()
-
-    for (let i = 0; i < navlinks.length; i++) {
-        navlinks[i].classList.add("scrolled")
-    }
+  for (let i = 0; i < navlinks.length; i++) {
+    navlinks[i].classList.add("scrolled");
+  }
 }
-
 
 window.onscroll = () => {
-    if (this.scrollY > 430) {
-    changeStyle()
-    
-        for (let i = 0; i < navlinks.length; i++) {
-            navlinks[i].classList.add("scrolled")
-        }
-    } else {
-        nav.classList.remove("navbarScroll")
-        nav.classList.remove("shadow")
-        for (let i = 0; i < navlinks.length; i++){
-  
-            navlinks[i].classList.remove("scrolled")
-            navbarToggler.classList.remove("navbar-toggler-scroll")
-         
-        }
+  if (this.scrollY > 430) {
+    changeStyle();
+
+    for (let i = 0; i < navlinks.length; i++) {
+      navlinks[i].classList.add("scrolled");
     }
-
-}
-
+  } else {
+    nav.classList.remove("navbarScroll");
+    nav.classList.remove("shadow");
+    for (let i = 0; i < navlinks.length; i++) {
+      navlinks[i].classList.remove("scrolled");
+      navbarToggler.classList.remove("navbar-toggler-scroll");
+    }
+  }
+};
